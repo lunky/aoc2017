8e6bcfa906633aebf65673039fb8c77778fefd56
Quinn Wilson <github@midworld.org>
2017-12-04 04:48:43 +0000

    got the answer but tests fail

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,24 +2,32 @@ module Day3
     ( 
     day3
    ,day3b
+   ,route
     )
     where
---    1	2 11 28 53 86
---    0 1 9 17 33
+import Data.List
 
 -- start at (0,0)
 day3 :: Int -> Int
-day3 input = (\(x,y)-> (abs x + abs y)-1) $ foldr(\x acc -> move x acc) (0,0) $ route input
+day3 input = (\(x,y)-> (abs x + abs y)-1) $ foldr(\x acc -> move x acc) (0,0) $ take input $ route
 
-circle = foldr (\(x,y) xs -> x:(y:xs) ) [] $ zip [1..] [1..]
-set =  cycle ["right","up","left","down"]
-instructions = zip circle (cycle set)
 
---travel :: String -> (Int,Int) -> Int -> (Int, Int)
---travel direction location 0 = ()
-route :: Int -> [String]
-route len = take len $ concat $ map (\(dir,y) -> map (\x -> dir) y ) $ zip set $ tupleToList $ (\a -> zip a a) $ map (\y -> (take y $ repeat 1) ) $ [x | x <- [1..]]
---route = zip set $ tupleToList $ (\a -> zip a a) $ map (\y -> (take y $ repeat 1) ) $ [x | x <- [1..]]
+route :: [String]
+route = concat $ 
+        map (\(dir,y) -> map (\x -> dir) y ) $ 
+        zip set $ tupleToList $ (\a -> zip a a) $ 
+        map (\y -> (take y $ repeat 1) ) [1..]
+    where set =  cycle ["right","up","left","down"]
+
+adjacent :: (Int,Int) -> [(Int,Int)]
+adjacent (x,y) = map (\(a,b) -> (a+x,b+y))  [(x,y)| x <- [-1..1], y<-[-1..1]]
+
+rank :: (Int,Int) -> [((Int,Int),Int)] -> ((Int,Int),Int)
+rank (0,0) _ = ((0,0),1)
+rank pos prev = (\y -> (pos,y)) $ 
+                sum $ map snd $ 
+                intersectBy (\(pos1,rank1) (pos2, rank2) -> pos1==pos2)  
+                prev (map (\y->(y,99)) $ adjacent pos)
 
 tupleToList :: [(a,a)] -> [a]
 tupleToList ((a,b):xs) = a : b : tupleToList xs
@@ -29,8 +37,8 @@ move :: [Char] -> (Int, Int) -> (Int, Int)
 move "right" (x,y) = (x+1,y) 
 move "up"    (x,y) = (x,y+1)
 move "left"  (x,y) = (x-1,y)
-move _  (x,y) = (x,y-1)
+move "down"  (x,y) = (x,y-1)
 
     
 day3b :: Int -> Int
-day3b input = 0
+day3b input = (\(x,y)-> (abs x + abs y)-1) $ foldl'(\acc x -> move x acc) (0,0) $ take input $ route
--- test/Day3Spec.hs
+++ test/Day3Spec.hs
@@ -18,9 +18,21 @@ spec = do
             let input = 12
             day3 input `shouldBe` expected
     describe "Day3b" $ do
-        it "should give a response"  $ do 
-            let expected = 0
-            let input = 0
+        it "should give a response 1 1"  $ do 
+            let expected = 1
+            let input = 1
+            day3b input `shouldBe` expected
+        it "should give a response 2 1"  $ do 
+            let expected = 1
+            let input = 2
+            day3b input `shouldBe` expected
+        it "should give a response 3 2"  $ do 
+            let expected = 2
+            let input = 3
+            day3b input `shouldBe` expected
+        it "should give a response 4 4"  $ do 
+            let expected = 4
+            let input = 4
             day3b input `shouldBe` expected
             
         
