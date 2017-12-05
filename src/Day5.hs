44be2c99ceb9890eef866216239d5e21d280ab4b
Quinn Wilson <c9@midworld.org>
2017-12-05 22:26:37 +0000

    get it compiling

--- src/Day5.hs
+++ src/Day5.hs
@@ -5,34 +5,32 @@ module Day5
    ,move
     )
     where
-import Data.Vector
+import Data.Vector ((!), (//))
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed.Mutable as M
 
 day5 :: String -> Int
-day5 input = move 0 0 $ map (\y -> (read y)::Int ) $ lines input
+day5 input = move 0 0 $ V.fromList $ map (\y -> (read y)::Int ) $ lines input
 
-incAt :: Int -> [Int] -> [Int] -- increments element at index
-incAt index ls = a ++ ((el+1):b) where (a, (el:b)) = splitAt index ls
-decAt :: Int -> [Int] -> [Int] -- increments element at index
-decAt index ls = a ++ ((el-1):b) where (a, (el:b)) = splitAt index ls
-
+incAt :: Int -> V.Vector Int -> V.Vector Int -- increments element at index
+incAt index ls = ls V.// [(index, (ls ! index)+1)]
+decAt :: Int -> V.Vector Int -> V.Vector Int -- increments element at index
+decAt index ls = ls // [(index,(ls ! index)-1)]
+    
 --move :: Int -> Int -> Vector Int -> Int
-move :: Int -> Int -> [Int] -> Int
+move :: Int -> Int -> V.Vector Int -> Int
 move count start list 
     | start > (length list - 1) = count
-    | otherwise = move (count+1) (start + list!!start) (incAt start list)
-        where nextStart = (start + list!!start)
+    | otherwise = move (count+1) (start + list ! start) (incAt start list)
+        where nextStart = (start + list ! start)
         
-moveB :: Int -> Int -> Vector Int -> Int
+moveB :: Int -> Int -> V.Vector Int -> Int
 moveB count start list 
     | start > (length list - 1) = count
-    | otherwise = moveB (count+1) (start + list!!start) (operation start list)
-        where nextStart = (start + list!!start)
-              operation = if(list!!start>=3) then decAt else incAt
+    | otherwise = moveB (count+1) (start + list ! start) (operation start list)
+        where nextStart = (start + list ! start)
+              operation = if(list ! start >=3) then decAt else incAt
 
 day5b :: String -> Int
-day5b input = moveB 0 0 $ fromList $ map (\y -> (read y)::Int ) $ lines input
+day5b input = moveB 0 0 $ V.fromList $ map (\y -> (read y)::Int ) $ lines input
 
-
--- current index
--- list 
--- 
