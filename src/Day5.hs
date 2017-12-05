da55d5a8a84dfd1a2cba1ec1ffccdcc4c72e0399
Quinn Wilson <github@midworld.org>
2017-12-05 12:41:28 +0000

    test

--- src/Day5.hs
+++ src/Day5.hs
@@ -5,6 +5,8 @@ module Day5
    ,move
     )
     where
+import Data.Vector
+
 day5 :: String -> Int
 day5 input = move 0 0 $ map (\y -> (read y)::Int ) $ lines input
 
@@ -13,13 +15,14 @@ incAt index ls = a ++ ((el+1):b) where (a, (el:b)) = splitAt index ls
 decAt :: Int -> [Int] -> [Int] -- increments element at index
 decAt index ls = a ++ ((el-1):b) where (a, (el:b)) = splitAt index ls
 
+--move :: Int -> Int -> Vector Int -> Int
 move :: Int -> Int -> [Int] -> Int
 move count start list 
     | start > (length list - 1) = count
     | otherwise = move (count+1) (start + list!!start) (incAt start list)
         where nextStart = (start + list!!start)
         
-moveB :: Int -> Int -> [Int] -> Int
+moveB :: Int -> Int -> Vector Int -> Int
 moveB count start list 
     | start > (length list - 1) = count
     | otherwise = moveB (count+1) (start + list!!start) (operation start list)
@@ -27,7 +30,7 @@ moveB count start list
               operation = if(list!!start>=3) then decAt else incAt
 
 day5b :: String -> Int
-day5b input = moveB 0 0 $ map (\y -> (read y)::Int ) $ lines input
+day5b input = moveB 0 0 $ fromList $ map (\y -> (read y)::Int ) $ lines input
 
 
 -- current index
