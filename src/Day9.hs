09875eb8440f7a333c8b0beaa460aedeb26ac8f3
Quinn Wilson <github@midworld.org>
2017-12-16 04:50:26 +0000

    Day9 done

--- src/Day9.hs
+++ src/Day9.hs
@@ -4,10 +4,29 @@ module Day9
    ,day9b
     )
     where
-
 day9 :: String -> Int
-day9 input = 0
+day9 input = parseGroup 0 0 input
     
 day9b :: String -> Int
-day9b input = 0
+day9b input = countGarbage 0 False input
+
+parseGroup :: Int -> Int -> String -> Int
+parseGroup count score ('{':input) = parseGroup (count + 1) (score + count + 1) input
+parseGroup count score ('}':input) = parseGroup (count - 1) score  input
+parseGroup count score ('<':input) = parseGarbage count score  input
+    where parseGarbage :: Int -> Int -> String -> Int
+          parseGarbage count score ('!':_:input) = parseGarbage count score input
+          parseGarbage count score ('>':input) = parseGroup count score input
+          parseGarbage count score (_:input) = parseGarbage count score input
+          parseGarbage _ score [] = score
+          
+parseGroup count score  (_:input) = parseGroup count score  input
+parseGroup count score [] = score
     
+
+countGarbage count inBlock ('!':_:input) = countGarbage count inBlock input
+countGarbage count True ('<':input) = countGarbage (count+1) True input
+countGarbage count inBlock ('<':input) = countGarbage count True input
+countGarbage count inBlock ('>':input) = countGarbage (count) False input
+countGarbage count inBlock (_:input) = countGarbage (if inBlock then (count +1) else count) inBlock input
+countGarbage count _ [] = count 
--- test/Day9Spec.hs
+++ test/Day9Spec.hs
@@ -8,7 +8,64 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "Day9" $ do
-        it "should something"  $ do 
-            let input = ""
+        it "should parse single"  $ do 
+            let input = "{}"
             let expected = 1
             day9 input `shouldBe` expected
+        it "should parse triple"  $ do 
+            let input = "{{{}}}"
+            let expected = 6
+            day9 input `shouldBe` expected
+        it "should parse nested"  $ do 
+            let input = "{{},{}}"
+            let expected = 5
+            day9 input `shouldBe` expected
+        it "should parse nested"  $ do 
+            let input = "{{{},{},{{}}}}"
+            let expected = 16
+            day9 input `shouldBe` expected
+        it "should parse nested"  $ do 
+            let input = "{<a>,<a>,<a>,<a>}"
+            let expected = 1
+            day9 input `shouldBe` expected
+        it "should parse nested"  $ do 
+            let input = "{{<ab>},{<ab>},{<ab>},{<ab>}}"
+            let expected = 9
+            day9 input `shouldBe` expected
+        it "should parse nested"  $ do 
+            let input = "{{<!!>},{<!!>},{<!!>},{<!!>}}"
+            let expected = 9
+            day9 input `shouldBe` expected
+        it "should parse nested"  $ do 
+            let input = "{{<a!>},{<a!>},{<a!>},{<ab>}}"
+            let expected = 3
+            day9 input `shouldBe` expected
+    describe "Day9b" $ do
+        it "should parse empty"  $ do 
+            let input = "<>"
+            let expected = 0
+            day9b input `shouldBe` expected
+        it "should parse chars"  $ do 
+            let input = "<random characters>"
+            let expected = 17
+            day9b input `shouldBe` expected
+        it "should parse unbalanced"  $ do 
+            let input = "<<<<>"
+            let expected = 3
+            day9b input `shouldBe` expected
+        it "should parse chars"  $ do 
+            let input = "<{!>}>"
+            let expected = 2
+            day9b input `shouldBe` expected
+        it "should parse double !"  $ do 
+            let input = "<!!>"
+            let expected = 0
+            day9b input `shouldBe` expected
+        it "should parse triple bang"  $ do 
+            let input = "<!!!>>"
+            let expected = 0
+            day9b input `shouldBe` expected
+        it "should parse escaped"  $ do 
+            let input = "<{o\"i!a,<{i<a>"
+            let expected = 10
+            day9b input `shouldBe` expected
