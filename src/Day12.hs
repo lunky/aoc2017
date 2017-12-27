7beb028cbeb2a101f7ecf5bcb34abbf1422a7850
Quinn Wilson <github@midworld.org>
2017-12-27 05:21:36 +0000

    Day12 done

--- src/Day12.hs
+++ src/Day12.hs
@@ -4,10 +4,27 @@ module Day12
    ,day12b
     )
     where
+import Data.Maybe (fromJust)
+import Data.List (foldl',find,partition)
+import qualified Data.Set as Set
 
 day12 :: String -> Int
-day12 input = 0
+day12 input = fromJust $ fmap length $ find (Set.member "0") $ foldl' connect [] $ parseTree input
+
+parseTree :: String -> [(String,[String])]
+parseTree input = map parseLine $ lines input
+
+parseLine :: String -> (String,[String])
+parseLine input = (\list -> (head list, (tail $ tail list)) ) $ map trimTrailingComma $words input 
+
+trimTrailingComma :: String -> String
+trimTrailingComma  text = if (last text==',') then init text else text
+
+connect :: Ord a => [Set.Set a] -> (a, [a]) -> [Set.Set a]
+connect sets (key, children) = Set.unions (theSet : connected) : isolated
+  where theSet = Set.fromList (key : children)
+        (isolated, connected) = partition (Set.null . Set.intersection theSet) sets
 
 day12b :: String -> Int
-day12b input = 0
+day12b input = length . foldl' connect [] $ parseTree input
 
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -7,6 +7,16 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "day12 move" $ do
+  describe "day12" $ do
     it "run a basic test" $ do 
       1 `shouldBe` 1
+  describe "day12" $ do
+    it "should behave like the example" $ do 
+      let input = "0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5\n"
+      let expected = 6
+      day12 input `shouldBe` expected
+  describe "day12b" $ do
+    it "should behave like the example" $ do 
+      let input = "0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5\n"
+      let expected = 2
+      day12b input `shouldBe` expected
