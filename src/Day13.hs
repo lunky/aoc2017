fd62260a57f6df730dad087d633a46c0f7844465
Quinn Wilson <github@midworld.org>
2017-12-31 02:35:28 +0000

    Day13b done!

--- src/Day13.hs
+++ src/Day13.hs
@@ -14,23 +14,21 @@ day13 input = sum $ map (\(x,y,_)-> x*y)
                 $ catMaybes
                 $ map (\(lst, index) -> find (\(y,_,_) -> y==index ) lst) 
                 $ zip (day13' 0 input) [0..]
-                
-day13' start input = map (\y -> runPicoSecond y layers) [start..start+layerCount] 
-    where   layers = parseInput input
-            layerCount = maximum $ map fst layers
+    where  day13' :: Int -> String -> [[(Int,Int,Int)]]                
+           day13' start input = map (\y -> runPicoSecond y layers) [start..start+layerCount] 
+           layers = parseInput input
+           layerCount = maximum $ map fst layers
+           runPicoSecond :: Int -> [(Int,Int)] ->  [(Int,Int,Int)]
+           runPicoSecond picoSecond layers = map (\layer -> setLayer picoSecond layer) layers
             
---day13b' :: Int -> String -> Int
-day13b' start input = length
-                $ filter(\(_,_,y) -> y==1) 
-                $ catMaybes
-                $ map (\(lst, index) -> find (\(y,_,_) -> y==index ) lst) 
-                $ zip (day13' start input) [0..]
-                
 day13b :: String -> Int
-day13b input = snd $head $ dropWhile (\y -> (fst y)/=0) $  zip ( map (\y -> day13b' y input) [1..]) [1..] 
+day13b input = snd $head $ dropWhile (\(y,_) -> y/=True) $ map (\y -> (tick y input,y)) [1..]
 
-runPicoSecond :: Int -> [(Int,Int)] ->  [(Int,Int,Int)]
-runPicoSecond picoSecond layers = map (\layer -> setLayer picoSecond layer) layers
+tick :: Int -> String -> Bool
+tick picoSecond input = all (==False) $ map (\(layer,range) -> caught (picoSecond+layer) range ) $ parseInput input
+
+caught :: Int -> Int -> Bool
+caught picoSecond range =   (oscillateIndex range picoSecond) == 1
 
 setLayer :: Int -> (Int, Int) -> (Int,Int,Int)
 setLayer picoSecond (layer,range) =  (layer, range, (oscillate range) !! (picoSecond))
@@ -42,6 +40,10 @@ oscillate range
     | range <=0 = []
     | range ==1 = cycle [1]
     | otherwise = cycle [x | x <- [1..range]++[range-1,range-2..2]]
+
+oscillateIndex :: Int -> Int -> Int
+oscillateIndex range index = (oscillate range) !! (index - ((index `div` range') * range'))
+    where range' = (range-1)*2
 
 parseInput :: String -> [(Int,Int)]
 parseInput = concatMap (fmap fst . filter ((== "") . snd)) . fmap parseLine . lines 
@@ -57,6 +59,3 @@ parseInput = concatMap (fmap fst . filter ((== "") . snd)) . fmap parseLine . li
             <*  skipSpaces
             <*> parseNumber
             <*  eof
-
-
-
