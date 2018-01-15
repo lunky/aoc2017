297f3ec9e64bdbcc1286f1cc453d8b9adcbd4a81
Quinn Wilson <github@midworld.org>
2018-01-15 14:55:35 +0000

    seems to be working, waiting for Day16b run

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,7 @@ import Day12
 import Day13
 import Day14
 import Day15
+import Day16
 
 import Data.List
 import Data.Function
@@ -23,94 +24,101 @@ readLines = fmap lines . readFile
 
 main :: IO ()
 main =  do  
-    contents <- readFile "input/1.txt"
-    let answer = show $ day1  (head $ lines $ contents)
-    putStrLn ("day1: " ++ answer)
-    let answer = show $ day1b (head $ lines $ contents)
-    putStrLn ("day1b: " ++ answer)
-    putStrLn "+++++++++++++++++++++++++++++"
-    contents <- readFile "input/2.txt"
-    let answer = show $ day2  contents
-    putStrLn ("day2: " ++ answer)
-    let answer = show $ day2b contents
-    putStrLn ("day2b: " ++ answer)
+--    contents <- readFile "input/1.txt"
+--    let answer = show $ day1  (head $ lines $ contents)
+--    putStrLn ("day1: " ++ answer)
+--    let answer = show $ day1b (head $ lines $ contents)
+--    putStrLn ("day1b: " ++ answer)
+--    putStrLn "+++++++++++++++++++++++++++++"
+------    contents <- readFile "input/2.txt"
+--    let answer = show $ day2  contents
+--    putStrLn ("day2: " ++ answer)
+--    let answer = show $ day2b contents
+--    putStrLn ("day2b: " ++ answer)
+--    
+--    contents <- readFile "input/4.txt"
+--    let answer = show $ day4  contents
+--    putStrLn ("day4: " ++ answer)
+--    let answer = show $ day4b contents
+--    putStrLn ("day4b: " ++ answer)
+--    
+----    contents <- readFile "input/5.txt"
+----    let answer = show $ day5  contents
+----    putStrLn ("day5: " ++ answer)
+--    putStrLn ("day5: " ++ "too slow to run")
+----    let answer = show $ day5b  contents
+----    putStrLn ("day5b: " ++ answer)
+--    putStrLn ("day5b: " ++ "too slow to run")
+--    
+--
+--    contents <- readFile "input/6.txt"
+--    let answer = show $ day6 contents
+--    putStrLn ("day6: " ++ answer)
+--    let answer = show $ day6b contents
+--    putStrLn ("day6b: " ++ answer)
+--    
+--    contents <- readFile "input/7.txt"
+--    let answer = show $ day7 contents
+--    putStrLn ("day7: " ++ answer)
+--    let answer = show $ day7b contents
+--    putStrLn ("day7b: " ++ answer)
+--    
+--    contents <- readFile "input/8.txt"
+--    let answer = show $ day8 contents
+--    putStrLn ("day8: " ++ answer)
+--    let answer = show $ day8b contents
+--    putStrLn ("day8b: " ++ answer)
+--    
+--    contents <- readFile "input/9.txt"
+--    let answer = show $ day9 contents
+--    putStrLn ("day9: " ++ answer)
+--    let answer = show $ day9b contents
+--    putStrLn ("day9b: " ++ answer)
+--    
+--    contents <- readFile "input/10.txt"
+--    let answer = show $ day10 contents [0..255]
+--    putStrLn ("day10: " ++ answer)
+--    let answer = show $ day10b contents [0..255]
+--    putStrLn ("day10b: " ++ answer)
+--    
+--    contents <- readFile "input/11.txt"
+--    let answer = show $ day11 contents 
+--    putStrLn ("day11: " ++ answer)
+--    let answer = show $ day11b contents
+--    putStrLn ("day11b: " ++ answer)
+--
+--    contents <- readFile "input/12.txt"
+--    let answer = show $ day12 contents 
+--    putStrLn ("day12: " ++ answer)
+--    let answer = show $ day12b contents
+--    putStrLn ("day12b: " ++ answer)
+--    
+--    contents <- readFile "input/13.txt"
+--    let answer = show $ day13 contents 
+--    putStrLn ("day13: " ++ answer)
+--    --let answer = show $ day13b contents
+--    --putStrLn ("day13b: " ++ answer)
+--    putStrLn ("day13b: " ++ "too slow to run")
+--    
+----    let contents = "jxqlasbh"
+----    let answer = show $ day14 contents 
+----    putStrLn ("day14: " ++ answer)
+--    putStrLn ("day14: " ++ "too slow to run")
+----    let answer = show $ day14b contents
+----    putStrLn ("day14b: " ++ answer)
+--    putStrLn ("day14b: " ++ "too slow to run")
+--    
+----    contents <- readFile "input/15.txt"
+----    let answer = show $ day15 40000000 contents 
+----    putStrLn ("day15: " ++ answer)
+--    putStrLn ("day15b: " ++ "too slow to run")
+----    let answer = show $ day15b 5000000 contents
+----    putStrLn ("day15b: " ++ answer)
+--    putStrLn ("day15b: " ++ "too slow to run")
+--    
+    contents <- readFile "input/16.txt"
+    let answer = show $ day16 ['a'..'p'] contents 
+    putStrLn ("day16: " ++ answer)
+    let answer = show $ day16b 1000000000 ['a'..'p'] contents
+    putStrLn ("day16b: " ++ answer)
     
-    contents <- readFile "input/4.txt"
-    let answer = show $ day4  contents
-    putStrLn ("day4: " ++ answer)
-    let answer = show $ day4b contents
-    putStrLn ("day4b: " ++ answer)
-    
---    contents <- readFile "input/5.txt"
---    let answer = show $ day5  contents
---    putStrLn ("day5: " ++ answer)
-    putStrLn ("day5: " ++ "too slow to run")
---    let answer = show $ day5b  contents
---    putStrLn ("day5b: " ++ answer)
-    putStrLn ("day5b: " ++ "too slow to run")
-    
-
-    contents <- readFile "input/6.txt"
-    let answer = show $ day6 contents
-    putStrLn ("day6: " ++ answer)
-    let answer = show $ day6b contents
-    putStrLn ("day6b: " ++ answer)
-    
-    contents <- readFile "input/7.txt"
-    let answer = show $ day7 contents
-    putStrLn ("day7: " ++ answer)
-    let answer = show $ day7b contents
-    putStrLn ("day7b: " ++ answer)
-    
-    contents <- readFile "input/8.txt"
-    let answer = show $ day8 contents
-    putStrLn ("day8: " ++ answer)
-    let answer = show $ day8b contents
-    putStrLn ("day8b: " ++ answer)
-    
-    contents <- readFile "input/9.txt"
-    let answer = show $ day9 contents
-    putStrLn ("day9: " ++ answer)
-    let answer = show $ day9b contents
-    putStrLn ("day9b: " ++ answer)
-    
-    contents <- readFile "input/10.txt"
-    let answer = show $ day10 contents [0..255]
-    putStrLn ("day10: " ++ answer)
-    let answer = show $ day10b contents [0..255]
-    putStrLn ("day10b: " ++ answer)
-    
-    contents <- readFile "input/11.txt"
-    let answer = show $ day11 contents 
-    putStrLn ("day11: " ++ answer)
-    let answer = show $ day11b contents
-    putStrLn ("day11b: " ++ answer)
-
-    contents <- readFile "input/12.txt"
-    let answer = show $ day12 contents 
-    putStrLn ("day12: " ++ answer)
-    let answer = show $ day12b contents
-    putStrLn ("day12b: " ++ answer)
-    
-    contents <- readFile "input/13.txt"
-    let answer = show $ day13 contents 
-    putStrLn ("day13: " ++ answer)
-    --let answer = show $ day13b contents
-    --putStrLn ("day13b: " ++ answer)
-    putStrLn ("day13b: " ++ "too slow to run")
-    
---    let contents = "jxqlasbh"
---    let answer = show $ day14 contents 
---    putStrLn ("day14: " ++ answer)
-    putStrLn ("day14: " ++ "too slow to run")
---    let answer = show $ day14b contents
---    putStrLn ("day14b: " ++ answer)
-    putStrLn ("day14b: " ++ "too slow to run")
-    
---    contents <- readFile "input/15.txt"
---    let answer = show $ day15 40000000 contents 
---    putStrLn ("day15: " ++ answer)
-    putStrLn ("day15b: " ++ "too slow to run")
---    let answer = show $ day15b 5000000 contents
---    putStrLn ("day15b: " ++ answer)
-    putStrLn ("day15b: " ++ "too slow to run")
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -2,10 +2,66 @@ module Day16Spec where
 
 import Day16
 import Test.Hspec
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic.Mutable as MV
 
 spec :: Spec
 spec = do
-    describe "Lib" $ do
-        it "should run a noop test" $
-            1 `shouldBe` 1
-    --describe "Day16" $ do
+    describe "spin" $ do
+        it "should spin" $ do
+            let input = 1 
+            let input2 = "abcde"
+            let expected = "eabcd"
+            spin input input2 `shouldBe` expected
+            
+    describe "exchange" $ do
+        it "should exchange" $ do 
+            let input = (3,4)
+            let input2 = "eabcd"
+            let expected = "eabdc"
+            exchange input input2 `shouldBe` expected
+            
+    describe "partner" $ do
+        it "should partner" $ do 
+            let input = ('e','b')
+            let input2 = "eabdc"
+            let expected = "baedc"
+            partner input input2 `shouldBe` expected
+    describe "parseLine" $ do
+        it "should parse string into Instructions" $ do 
+            let input = "s1,x3/4,pe/b"
+            let expected = Just [Spin 1, Exchange 3 4, Partner 'e' 'b']
+            parseLine input `shouldBe` expected
+    describe "Day16" $ do
+        it "should execute all your intructions" $ do
+            let input = "abcde"
+            let input2 = "s1,x3/4,pe/b"
+            let expected = "baedc"
+            day16 input input2 `shouldBe` expected
+    describe "Day16b" $ do
+        it "should execute instructions 1B times" $ do
+            let input = "abcde"
+            let input2 = "s1,x3/4,pe/b"
+            let expected = "ceadb"
+            day16b 2 input input2 `shouldBe` expected
+
+    describe "spinv" $ do
+        it "should spinv" $ do
+            let input = 1 
+            let input2 = V.fromList "abcde"
+            let expected = V.fromList "eabcd"
+            spinv input input2 `shouldBe` expected
+            
+    describe "exchangev" $ do
+        it "should exchange" $ do 
+            let input = (3,4)
+            let input2 = V.fromList "eabcd"
+            let expected = V.fromList "eabdc"
+            exchangev input input2 `shouldBe` expected
+            
+    describe "partnerv" $ do
+        it "should partnerv" $ do 
+            let input = ('e','b')
+            let input2 = V.fromList "eabdc"
+            let expected = V.fromList "baedc"
+            partnerv input input2 `shouldBe` expected
