fd04e98337e0780f559cfd17e2607248ff49445d
Quinn Wilson <c9@midworld.org>
2018-01-27 05:55:59 +0000

    Day17 done \o/

--- src/Day17.hs
+++ src/Day17.hs
@@ -25,6 +25,15 @@ insertAt z y xs = as ++ (y:bs)
                   where (as,bs) = Prelude.splitAt z xs
 
 day17b :: Int -> Int 
-day17b increment = (\(_,list)->list !!  1) $ foldl' advance' (0,[0]) [50000000..50000001]
-    where advance' = advance increment
---day17 increment = (\(idx, list)-> list !! (idx+1)) $ foldl' advance' (0,[0]) [1..2017]
+day17b increment = snd $ foldl' advance' (0,0) [1..50000000]
+    where advance' = advanceB increment
+-- this version doesn't even track a list, that was using all the memory on my laptop
+-- even though this version doesn't use a list it still uses a tonne of ram for some reason
+-- I don't know why. TODO -- fix RAM usage
+-- maybe a pure recursive version?
+advanceB increment (index,element1) value = (next,if(next==1) then (value) else element1)
+    where 
+          next 
+            | indexNIncrement >= value = 1 + (indexNIncrement) `rem` (value)
+            | otherwise = 1 + indexNIncrement
+          indexNIncrement = index + increment
